4a4210976c0d49825821330f8fb4f212ca8e4201
Richard Shackleton <richard.shackleton@idhl.co.uk>
2021-12-22 16:13:24 +0000

    Test with redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path((?!unsupported.html$).*)',
+        destination: '/unsupported.html',
+        has: [
+          {
+            type: 'header',
+            key: 'User-Agent',
+            value: '(.*Trident.*)',
+          },
+        ],
+        permanent: false,
+      },
+    ];
+  },
 }
